61eaf2f3bffae2ab6a52551146309d08a9b2bfd5
Juan <juanvpitanga@gmail.com>
2022-07-01 09:39:54 -0300

    Pessoa.kt

--- digitalinnovation-main/api-collections/src/main/kotlin/one/digitalinovation/digioonebank/Pessoa.kt
+++ digitalinnovation-main/api-collections/src/main/kotlin/one/digitalinovation/digioonebank/Pessoa.kt
@@ -1,13 +1,19 @@
 package one.digitalinovation.digioonebank
 
 class Pessoa {
-    var nome: String = "Juan"
+   var nome: String = "Juan"
+
     var cpf: String =  "033.637.392-90"
-            
+    private set
 }
+
+
 fun main() {
     val Juan = Pessoa()
 
+    println(Juan)
     println(Juan.nome)
     println(Juan.cpf)
+
+
 }
